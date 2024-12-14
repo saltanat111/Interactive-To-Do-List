31b195eb053233a26662a3927d05551a7e78984a
Saltanat Kydykbekova <saltanat.kydykbekova1@gmail.com>
2024-12-14 14:23:20 +0600

    feature 'filter tasks(all, complete, incomplete)'

--- to do list/to-do.js
+++ to do list/to-do.js
@@ -13,30 +13,78 @@ function addTask(){
         let span = document.createElement("span");
         span .innerHTML = "\u00d7";
         li.appendChild(span);
+        saveLocalTodos(inputBox.value);
+
     }
     inputBox.value = '';
-    saveData();
+}
+//delete task, complete task
+listContainer.addEventListener("click", function(e){
+                                if(e.target.tagName === "LI"){
+                                    e.target.classList.toggle("checked");
+                                    saveLocalTodos();
+                                }
+                                else if(e.target.tagName === "SPAN"){
+                                    e.target.parentElement.remove();
+                                    saveLocalTodos();
+                                }
+                            },false);
+
+function saveLocalTodos() {
+    const todos = [];
+    document.querySelectorAll("li").forEach((li) => {
+        todos.push({
+            text: li.firstChild.textContent,
+            completed: li.classList.contains("checked")
+        });
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-listContainer.addEventListener("click", function(e)
-                                        {
-                                            if(e.target.tagName === "LI"){
-                                                e.target.classList.toggle("checked");
-                                                saveData();
-                                            }
-                                            else if(e.target.tagName === "SPAN"){
-                                                e.target.parentElement.remove();
-                                                saveData();
-                                            }
-                                        },
-                                    false);
+function showTask() {
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    todos.forEach((todo) => {
+        let li = document.createElement("li");
+        li.innerHTML = todo.text;
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-}
+        if (todo.completed) {
+            li.classList.add("checked");
+        }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data")
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+        listContainer.appendChild(li);
+    });
 }
 
 showTask();
+
+const filterDropdown = document.getElementById("filter-todo");
+filterDropdown.addEventListener("change", () => filterTasks(filterDropdown.value));
+
+function filterTasks(filter) {
+    const todos = document.querySelectorAll("li");
+    todos.forEach((todo) => {
+        switch (filter) {
+            case "all":
+                todo.style.display = "flex"; 
+                break;
+            case "completed":
+                if (todo.classList.contains("checked")) {
+                    todo.style.display = "flex"; 
+                } else {
+                    todo.style.display = "none"; 
+                }
+                break;
+            case "incomplete":
+                if (!todo.classList.contains("checked")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
